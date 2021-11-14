51449b8d783b11a7a4cad663e031f98fa07ae3c5
aaron313 <42015624+aaron313@users.noreply.github.com>
2021-11-14 09:55:52 -0700

    Update test_air_quality_and_birth_weight_analysis.py
    Added in some unittest testing the dataclass object, to see if the @dataclass really worked, there is no logic to test in the BirthStatsObject() since I handle everything in pandas

--- test_air_quality_and_birth_weight_analysis.py
+++ test_air_quality_and_birth_weight_analysis.py
@@ -34,6 +34,51 @@ class TestImport_AirQuality_Data(unittest.TestCase):
         self.assertRaises(ValueError, obj.best_air_quality_in_state, "Japan")
 
         assert best_air_washington == "Clark"
+  
+class TestBirthObj(unittest.TestCase):
+
+    def test_lt(self):
+        high  = (2016,"Davis County", "UT", 7.9)
+        low = (2016,"Davis County", "UT", 7.4)
+        message = "high is not less than low"
+        self.assertLess(low,high,message)
+
+        high  = (2016,"Weber County", "UT", 9.9)
+        low = (2016,"Weber County", "UT", 1.4)
+        message = "high is not less than low"
+        self.assertLess(low,high,message)
+
+        high  = (2017,"Weber County", "UT", 9.9)
+        low = (2016,"Weber County", "UT", 9.9)
+        message = "high is not less than low"
+        self.assertLess(low,high,message)
+
+    def test_gt(self):
+        high  = (2016,"Davis County", "UT", 7.9)
+        low = (2016,"Davis County", "UT", 7.4)
+        message = "high is not greater than low"
+        self.assertGreater(high,low ,message)
+
+        high  = (2016,"Weber County", "UT", 7.5)
+        low = (2016,"Weber County", "UT", 7.49999)
+        message = "high is not greater than low"
+        self.assertGreater(high,low ,message)
+
+        high  = (2018,"Weber County", "UT", 7.555)
+        low = (2016,"Weber County", "UT", 7.555)
+        message = "high is not greater than low"
+        self.assertGreater(high,low ,message)
+
+    def test_eq(self):
+        ob1  = (2016,"Davis County", "UT", 7.9)
+        ob2 = (2016,"Davis County", "UT", 7.9)
+        message = "ob1 doest not equal ob2"
+        self.assertEqual(ob1,ob2,message)
+
+        ob1  = (2016,"Cool guy County", "UT", 7.999)
+        ob2 = (2016,"Cool guy County", "UT", 7.999)
+        message = "ob1 doest not equal ob2"
+        self.assertEqual(ob1,ob2,message)
                 
 if __name__ == '__main__':
     unittest.main()
